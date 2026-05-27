f418fdbc462559d8c5cc533341c423edfe3a03e0
sensrast <sensrast@gmail.com>
2026-05-27 20:41:08 +0530

    feat(auto-accept): handlers/callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -26,6 +26,7 @@ from handlers.admin.channel_mgmt import channels_panel, channel_view as admin_ch
 from handlers.admin.booking_mgmt import bookings_panel
 from handlers.admin.platform_settings import settings_panel, pricing_settings_panel, forcesub_panel, joinreq_panel
 from handlers.admin.pricing_engine import pricing_panel, pricing_recalc
+from handlers.admin.autoaccept import autoaccept_panel, autoaccept_toggle
 
 log = logging.getLogger(__name__)
 
@@ -90,6 +91,7 @@ ROUTES = {
     "common:fsub:verify": force_sub_verify,
     "admin:pricing": pricing_panel,
     "admin:pricing:recalc": pricing_recalc,
+    "admin:aar": autoaccept_panel,
 }
 
 PREFIX_ROUTES = [
@@ -119,6 +121,8 @@ PREFIX_ROUTES = [
     ("admin:user:", user_view),
     ("admin:ch:sus:", channel_suspend),
     ("admin:ch:", admin_channel_view),
+    ("admin:aar:p:", autoaccept_panel),
+    ("admin:aar:t:", autoaccept_toggle),
 ]
 
 async def callback_router(update, context):
